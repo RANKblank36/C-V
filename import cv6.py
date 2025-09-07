2332b803e60d4bbb63e272ddfd4d95321c770090
RANKblank36 <tendosana@gmail.com>
2025-09-07 18:29:20 +0200

    Update import cv6.py

--- import cv6.py
+++ import cv6.py
@@ -16,12 +16,14 @@ while True:
 
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors=5, minsize=(30, 30))
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors=5, minSize=(30, 30))
 
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-    font = cv2.FONT_HERSHEY_SIMPLEXcv2.putText(frame, f'People Count: {len(faces)}'(10, 30), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    cv2.putText(frame, f'People Count: {len(faces)}', (10, 30), font, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
     cv2.imshow('Face Tracking and Counting', frame)
 
